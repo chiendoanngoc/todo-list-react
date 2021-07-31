d9f0989985a0f012df494c68604c4ede82bbc00f
chien <chiendoanngoc1705@gmail.com>
2021-07-31 22:26:57 +0700

    middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
-import { createStore, combineReducers } from "redux";
-import todoReducer from './todo';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import todoReducer from "./todo";
 
 const reducer = combineReducers({
-    todo: todoReducer
+  todo: todoReducer,
 });
 
-export default createStore(reducer);
+const myMiddleware = store => next => action => {
+    if (action.type === "ADD_TODO" && action.payload === "fuck") {
+        action.payload = "****";
+    }
+    return next(action);
+}
+
+export default createStore(reducer, applyMiddleware(myMiddleware));
